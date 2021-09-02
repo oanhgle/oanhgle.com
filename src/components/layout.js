0b40bc69da7d2296651e2a566e5953e11080fcd5
Oanh Le <oanhle1@usf.edu>
2021-09-02 02:38:31 -0400

    (update) hover scale on email

--- src/components/layout.js
+++ src/components/layout.js
@@ -15,7 +15,7 @@ const Layout = ({pageTitle, children}) => {
         <div className="vl mt-5"></div>
       </div>
       <div className="fixed items-center bottom-0 right-20 hidden md:block">
-        <a href="mailto:oanhngle@gmail.com" className="aside font-mono text-sm text-gray-500 hover:text-primary">oanhngle@gmail.com</a>
+        <a href="mailto:oanhngle@gmail.com" className="aside font-mono text-sm text-gray-500 hover:text-primary transform transition duration-500 hover:scale-105">oanhngle@gmail.com</a>
         <div className="vl mt-5"></div>
       </div>
       <div>
