9ef3ca0ce5054e865a38c39b1882c7549e23b3d2
Oanh Le <oanhle1@usf.edu>
2021-09-02 17:50:20 -0400

    (fix) warning invalid dom

--- src/components/header.js
+++ src/components/header.js
@@ -7,7 +7,7 @@ const Header = () => {
             <div className="container m-auto sm:px-12 md:px-20 max-w-screen-xl px-15 py-40 mt-40 flex flex-col lg:flex items-center lg:justify-center lg:space-x-6 gap-7">
                 <h1 className="animate-bounce font-mono text-gray-500">Hi, my name is</h1>
                     <svg width="100%" height="100%">
-                        <text x="50%" y="80%" text-anchor="middle"> Oanh Le </text>
+                        <text x="50%" y="80%" textAnchor="middle"> Oanh Le </text>
                     </svg>
                 <h1 className="text-xl text-gray-500 font-bold">I'm a future Software Engineer</h1>
                 {/* <h1 className="animate-bounce text-xl text-gray-500 font-bold">see what i/m doing</h1> */}
